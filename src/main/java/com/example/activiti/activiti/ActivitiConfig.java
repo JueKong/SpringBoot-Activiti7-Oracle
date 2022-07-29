3676cd75b7e64b19ef65e2d764d5c13e47ca7e6a
JueKong <1922359708@qq.com>
2022-07-29 15:08:53 +0800

    监听器

---
+++ src/main/java/com/example/activiti/activiti/ActivitiConfig.java
@@ -0,0 +1,37 @@
+package com.edu.system.activiti;
+
+import com.edu.system.activiti.listener.ActivitiProcessCompleteListener;
+import com.google.common.collect.Lists;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.delegate.event.ActivitiEventListener;
+import org.activiti.engine.delegate.event.ActivitiEventType;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.activiti.spring.boot.ProcessEngineConfigurationConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author syl
+ * @Date 2022/7/29 12:15
+ * @Description
+ */
+
+@Configuration
+public class ActivitiConfig implements ProcessEngineConfigurationConfigurer {
+
+    @Autowired
+    private ActivitiProcessCompleteListener activitiProcessCompleteListener;
+
+    @Override
+    public void configure(SpringProcessEngineConfiguration configuration) {
+        Map<String, List<ActivitiEventListener>> activitiEventListenerMap = new HashMap<>();
+        activitiEventListenerMap.put(ActivitiEventType.PROCESS_COMPLETED.name(), Lists.newArrayList(activitiProcessCompleteListener));
+        configuration.setTypedEventListeners(activitiEventListenerMap);
+    }
+}
---
+++ src/main/java/com/example/activiti/activiti/listener/ActivitiProcessCompleteListener.java
@@ -0,0 +1,29 @@
+package com.edu.system.activiti.listener;
+import org.activiti.engine.delegate.event.ActivitiEvent;
+import org.activiti.engine.delegate.event.ActivitiEventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description 流程结束监听器
+ * @Date 22:44 2019/11/27
+ * @Param
+ * @return
+ **/
+@Component
+public class ActivitiProcessCompleteListener implements ActivitiEventListener {
+
+    @Override
+    public void onEvent(ActivitiEvent activitiEvent) {
+        System.out.println("部署流程ID：" + activitiEvent.getProcessDefinitionId());
+        System.out.println("当前流程ID：" + activitiEvent.getProcessInstanceId());
+        System.out.println("执行ID：" + activitiEvent.getExecutionId());
+        System.out.println(activitiEvent);
+        System.out.println("成功！");
+        //todo
+    }
+
+    @Override
+    public boolean isFailOnException() {
+        return false;
+    }
+}
